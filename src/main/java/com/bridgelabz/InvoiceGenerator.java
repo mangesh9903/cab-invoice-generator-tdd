a57edcd49087553c36c0bb4bb023c10ccdecc37f
Archanagowda23 <archanar249@gmail.com>
2022-05-12 15:25:15 +0530

    MODIFIED: Feature to Calculate Total Fare.

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-
     private static final double COST_PER_KM = 10.0;
     private static final double COST_PER_MIN = 1.0;
     private static final double MIN_FARE = 5;
